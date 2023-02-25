39d933edceeeb8e8691b3f9f9576ef1a9e276496
victorfouquet <victor.fouquet@gmail.com>
2023-02-25 13:53:03 +0100

    Add distribution for v1.0.0

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export { _Color } from "./interfaces/Color.interface";
+export { _ColorConverter } from "./interfaces/ColorConverter.interface";
+export { _HSLAcolor } from "./interfaces/HSLAcolor.interface";
+export { _RGBAcolor } from "./interfaces/RGBAcolor.interface";
+export { Color } from "./Color";
+export { ColorConverter } from "./ColorConverter";
+export { HSLAcolor } from "./HSLAcolor";
+export { RGBAcolor } from "./RGBAcolor";
